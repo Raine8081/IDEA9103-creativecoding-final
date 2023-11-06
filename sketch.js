2843a6372fd28954ec5c3ff98d1dcc0724f392f4
Raine8081 <145730541+Raine8081@users.noreply.github.com>
2023-11-06 19:05:35 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,171 @@
+let colors = [];
+let nums1 = [10, 100, 210, 320, 470, 500, 630, 700, 790];
+let nums2 = [30, 100, 210, 300, 400, 550, 630, 750, 790];
+let scaleX, scaleY;
+let walkerX;
+let walkerY;
+let stepSize = 10;
+let trail = [];
+let squareX;
+let squareY;
+let squareSize = 50; // 设置squareSize的初始值
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ 
+  noStroke();
+  background(250);
+  frameRate(20);
+
+  walkerX = width / 2;
+  walkerY = height / 2;
+  squareX = random(0, width - squareSize);
+  squareY = random(0, height - squareSize);
+
+  let grayColor = color(150);
+  let blueColor = color(21, 29, 176);
+  let redColor = color(161, 7, 2);
+
+  colors.push(grayColor);
+  colors.push(blueColor);
+  colors.push(redColor);
+
+  scaleX = windowWidth / 800;
+  scaleY = windowHeight / 800;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  scaleX = windowWidth / 800;
+  scaleY = windowHeight / 800;
+}
+
+function draw() {
+  background(250,250,250,25);
+
+  squareX += random(-stepSize, stepSize);
+  squareY += random(-stepSize, stepSize);
+  squareX = constrain(squareX, 0, width - squareSize);
+  squareY = constrain(squareY, 0, height - squareSize);
+
+  fill(255, 0, 0);
+  rect(squareX, squareY, squareSize, squareSize);
+
+  walkerX = lerp(walkerX, mouseX, 0.05);
+  walkerY = lerp(walkerY, mouseY, 0.05);
+
+  for (let i = 0; i < trail.length; i++) {
+    fill(random(colors));
+    rect(trail[i].x, trail[i].y, trail[i].width, trail[i].height);
+  }
+
+  for (let i = 1; i < 10; i++) {
+    if (i == 2 || i == 4 || i == 7) {
+      let x = width / 40 * i * 5;
+      let y = 0;
+      let w = width / 50;
+      let h = height / 2;
+      rect(x, y, w, h);
+      for (let k = 0; k < 15; k++) {
+        fill(random(colors));
+        rect(x, random(0, h), w, 20);
+      }
+    } else {
+      let x = width / 50 * i * 5;
+      let y = 0;
+      let w = width / 50;
+      let h = height;
+      fill(255, 229, 6);
+      rect(x, y, w, h);
+      for (let k = 0; k < 15; k++) {
+        fill(random(colors));
+        rect(x, random(0, h), w, 20);
+      }
+      fill(255, 229, 6);
+    }
+  }
+
+  fill(random(colors));
+  rect(walkerX, walkerY, random(200, 400), random(50, 600));
+
+  fill(random(colors));
+  rect(walkerX * 2, walkerY * 2, random(200, 400), random(200, 500));
+
+  fill(random(colors));
+  rect(walkerX * 5, walkerY * 5, random(200, 400), random(200, 700));
+
+  fill(random(colors));
+  rect(walkerX / 3, walkerY * 10, random(200, 400), random(600, 1800));
+
+  for (let j = 1; j < 8; j++) {
+    if (j == 1 || j == 5 || j == 6) {
+      let x = 0;
+      let y = height / 8 * j;
+      let w = width / 2;
+      let h = height / 40;
+      rect(x, y, w, h);
+      for (let k = 0; k < 15; k++) {
+        rect(random(0, w), y, 20, h);
+        fill(random(colors));
+      }
+    } else {
+      x = 0;
+      y = height / 8 * j;
+      w = width;
+      h = height / 40;
+      rect(x, y, w, h);
+      for (let k = 0; k < 15; k++) {
+        rect(random(0, w), y, 20, h);
+        fill(random(colors));
+      }
+      fill(255, 229, 6);
+    }
+  }
+
+  fill(random(colors));
+  rect(walkerX / 3, walkerY / 3, random(200, 400), random(200, 400));
+
+  for (let i = 0; i < 8; i++) {
+    noStroke();
+    fill(colors[1]);
+    rect(nums1[i] * scaleX, nums2[i] * 3 * scaleY, 100 * scaleX, 70 * scaleY);
+  }
+
+  for (let i = 0; i < 8; i++) {
+    noStroke();
+    fill(colors[2]);
+    rect(nums1[i] * scaleX, nums2[i] * 2 * scaleY, (100 + 50) * scaleX, 80 * scaleY);
+  }
+
+  for (let i = 0; i < 8; i++) {
+    noStroke();
+    fill(colors[2]);
+    rect(nums1[i] * 2 * scaleX, nums2[i] * scaleY, 60 * scaleX, 50 * scaleY);
+  }
+
+  for (let i = 0; i < 6; i++) {
+    noStroke();
+    fill(colors[2]);
+    rect(nums1[i] * 2 * scaleX, nums2[i] * scaleY, 60 * scaleX, 50 * scaleY);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    noStroke();
+    fill(colors[0]);
+    rect((nums1[i] * 2 + 20) * scaleX, (nums2[i] + 10) * scaleY, 30 * scaleX, 30 * scaleY);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    noStroke();
+    fill(255, 229, 6);
+    rect((nums1[i] + 200) * scaleX, (nums2[i] + 200) * scaleY, 40 * scaleX, 80 * scaleY);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    noStroke();
+    fill(colors[1]);
+    rect((nums1[i * 2] + 220) * scaleX, (nums2[i] + 400) * scaleY, 100 * scaleX, 80 * scaleY);
+  }
+}
